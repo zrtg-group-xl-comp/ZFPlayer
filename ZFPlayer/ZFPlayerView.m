422a8242570fc7559e0f7226836c8ac8f81b0398
smelloftime <herman8040@163.com>
2019-01-11 15:07:12 +0800

    fix: [播放] 播放失败loading还在显示

--- ZFPlayer/ZFPlayerView.m
+++ ZFPlayer/ZFPlayerView.m
@@ -1365,7 +1365,7 @@ typedef NS_ENUM(NSInteger, PanDirection){
     _state = state;
     // 控制Loading显示、隐藏
     // 进度开始更新/播放停止/播放错误 隐藏Loading,其他状态都显示
-    if (state == ZFPlayerStateStarted || state == ZFPlayerStateStopped || ZFPlayerStateFailed) {
+    if (state == ZFPlayerStateStarted || state == ZFPlayerStateStopped || state == ZFPlayerStateFailed) {
         [self.controlView zf_playerActivity: NO];
     } else {
         [self.controlView zf_playerActivity: YES];
